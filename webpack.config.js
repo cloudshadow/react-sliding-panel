69e3bd26726cb44d1d56ddf12bd9907e29966f2a
Cloud <cloudshadow.hjy@gmail.com>
2019-03-07 21:29:08 +0800

    change from webpack to rollup

--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,29 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: [
     // must be first entry to properly set public path
-    path.resolve(__dirname, 'src/index.js') // Defining path seems necessary for this to work consistently on Windows machines.
+    path.resolve(__dirname, './src/index.js') // Defining path seems necessary for this to work consistently on Windows machines.
   ],
   output: {
     path: path.resolve(__dirname, 'dist/'),
+    library: 'react-sliding-panel',
+    libraryTarget: 'umd',
     filename: 'index.js'
   },
-  plugins: [
-    // Hash the files using MD5 so that their names change when the content changes.
-    // new WebpackMd5Hash(),
-
-    // Generate an external css file with a hash in the filename
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "index.css",
-      // chunkFilename: "[id].css"
-    }),
-
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-      noInfo: true, // set to false to see a list of every file being bundled.
-      options: {
-        sassLoader: {
-          includePaths: [path.resolve(__dirname, 'src', 'scss')]
-        },
-        context: '/'
-      }
-    })
+  externals: [
+    {
+      react: 'react'
+    }
   ],
-  optimization: {
-    minimizer: [
-      // we specify a custom UglifyJsPlugin here to get source maps in production
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          compress: false,
-          ecma: 6,
-          mangle: true
-        },
-        sourceMap: true
-      })
-    ]
-  },
+  
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-        //type: 'javascript/auto',
-        loader: 'file-loader'
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        //type: 'javascript/auto',
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
-        //type: 'javascript/auto',
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        //type: 'javascript/auto',
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-      },
-      {
-        test: /\.(jpe?g|png|gif)$/i,
-        //type: 'javascript/auto',
-        loader: 'file-loader?name=[name].[ext]'
-      },
-      {
-        test: /\.ico$/,
-        //type: 'javascript/auto',
-        loader: 'file-loader?name=[name].[ext]'
-      },
-      {
-        test: /(\.css|\.scss|\.sass)$/,
-        //type: 'javascript/auto',
-        loaders: [MiniCssExtractPlugin.loader, 'css-loader?sourceMap', 'sass-loader?sourceMap']
-      }
+      {test: /\.(js|jsx)$/, use: ['babel-loader'], exclude: /node_modules/},
+      {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader','css-loader', 'sass-loader']}
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
   }
 };
